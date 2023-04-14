97a288eed91aaf9fbc1875128b8de639602c7f57
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-04-14 19:20:33 +0200

    feat: logger on submit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { TransactionService } from './transaction/transaction.service';
 import TransactionCreateDto from './transaction/dto/TransactionCreate.dto';
 
 @Controller({ host: 'public' })
 export class AppController {
+  private logger = new Logger(AppController.name);
   constructor(private transactionService: TransactionService) {}
 
   @Post('submit')
   async submit(@Body() transaction: TransactionCreateDto) {
+    this.logger.log(`Received transaction: ${JSON.stringify(transaction)}`);
     return this.transactionService.create(transaction);
   }
 }
