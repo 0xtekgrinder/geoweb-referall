596691da7f16bb9df199ec5dd473f74493963767
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-04-14 19:16:49 +0200

    feat: add everything required in admin module

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
+import { TransactionModule } from 'src/transaction/transaction.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   controllers: [AdminController],
+  imports: [TransactionModule, UserModule],
 })
 export class AdminModule {}
