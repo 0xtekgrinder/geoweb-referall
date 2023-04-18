4f4c5b0d80255e63ba396e291a304a78c40dab09
0xtekgrinder <72015889+0xtekgrinder@users.noreply.github.com>
2023-04-18 22:40:58 +0000

    feat: add logic to get payload from jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,13 @@
-import { Body, Controller, Logger, Post } from '@nestjs/common';
+import {
+  Controller,
+  Logger,
+  Post,
+  Headers,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { TransactionService } from './transaction/transaction.service';
-import TransactionCreateDto from './transaction/dto/TransactionCreate.dto';
+import * as ucans from '@ucans/ucans';
+import * as dagJose from 'dag-jose';
 
 @Controller({ host: 'public' })
 export class AppController {
@@ -8,8 +15,26 @@ export class AppController {
   constructor(private transactionService: TransactionService) {}
 
   @Post('submit')
-  async submit(@Body() transaction: TransactionCreateDto) {
-    this.logger.log(`Received transaction: ${JSON.stringify(transaction)}`);
-    return this.transactionService.create(transaction);
+  async submit(@Headers() headers: Record<string, string>) {
+    if (!headers.jwt) {
+      throw new UnauthorizedException('No JWT provided');
+    }
+    if (!headers.ucan) {
+      throw new UnauthorizedException('No UCAN provided');
+    }
+
+    const decodedJwt = dagJose.decode(
+      Buffer.from(headers.jwt),
+    ) as dagJose.DagJWS;
+    console.log(decodedJwt);
+
+    const parsed = JSON.parse(decodedJwt.payload);
+    console.log(parsed);
+
+    // ucans.verify(headers.ucan, {})
+    return this.transactionService.create({
+      referralId: parsed.referralId,
+      txHash: parsed.txHash,
+    });
   }
 }
