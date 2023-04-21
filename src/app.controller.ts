b867d7b71884204223e2b8b7f8eed5d465e80a46
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-04-21 02:26:33 +0200

    feat: verify everything on submit route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,11 +7,15 @@ import {
 } from '@nestjs/common';
 import { TransactionService } from './transaction/transaction.service';
 import * as ucans from '@ucans/ucans';
+import { ConfigService } from '@nestjs/config';
 
 @Controller({ host: 'public' })
 export class AppController {
   private logger = new Logger(AppController.name);
-  constructor(private transactionService: TransactionService) {}
+  constructor(
+    private transactionService: TransactionService,
+    private configService: ConfigService,
+  ) {}
 
   @Post('submit')
   async submit(@Headers() headers: Record<string, string>) {
@@ -22,17 +26,70 @@ export class AppController {
       throw new UnauthorizedException('No UCAN provided');
     }
 
-    const dagJose = await import('dag-jose');
-    const decodedJwt = dagJose.decode(Buffer.from(headers.jwt));
-    console.log(decodedJwt);
+    // const pkhDidResolver = import('pkh-did-resolver');
+    const keyDidResolver = import('key-did-resolver');
 
-    // const parsed = JSON.parse(decodedJwt.payload);
-    // console.log(parsed);
-// 
-    // // ucans.verify(headers.ucan, {})
-    // return this.transactionService.create({
-    //   referralId: parsed.referralId,
-    //   txHash: parsed.txHash,
-    // });
+    const dids = await import('dids');
+    const did = new dids.DID({
+      resolver: (await keyDidResolver).getResolver(),
+    });
+
+    const parsedJWS = await did.verifyJWS(JSON.parse(headers.jwt));
+
+    if (
+      !parsedJWS.payload ||
+      !parsedJWS.payload.txHash ||
+      !parsedJWS.payload.referralId
+    ) {
+      throw new UnauthorizedException('Invalid JWT payload');
+    }
+
+    const provider = import('ethers').then(
+      (ethers) =>
+        new ethers.ethers.JsonRpcProvider(
+          this.configService.get<string>('RPC_URL'),
+        ),
+    );
+
+    const transaction = await (
+      await provider
+    ).getTransaction(parsedJWS.payload.txHash);
+
+    if (!transaction) {
+      throw new UnauthorizedException('Invalid transaction hash');
+    }
+
+    // TODO check if transaction is from the user
+
+    const result = await ucans.verify(headers.ucan, {
+      audience: parsedJWS.kid,
+      requiredCapabilities: [
+        {
+          capability: {
+            with: {
+              scheme: 'https://geoweb.network/claim/did:pkh',
+              hierPart: `eip155:${this.configService.get<string>('CHAIN_ID')}:${
+                transaction.from
+              }`,
+            },
+            can: { namespace: 'http', segments: ['post'] },
+          },
+          rootIssuer: this.configService.get<string>('ROOT_ISSUER'),
+        },
+      ],
+    });
+
+    if (!result.ok) {
+      throw new UnauthorizedException('Invalid UCAN');
+    }
+
+    this.logger.log(
+      `Transaction ${parsedJWS.payload.txHash} from ${transaction.from} for referral ${parsedJWS.payload.referralId} was successfully verified`,
+    );
+
+    return this.transactionService.create({
+      referralId: parsedJWS.payload.referralId,
+      txHash: parsedJWS.payload.txHash,
+    });
   }
 }
