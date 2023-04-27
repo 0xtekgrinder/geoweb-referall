8caad934af4e07ef93f7572c3242b16ae070aab1
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-04-27 13:35:45 +0200

    feat: let the api documentation only in private port

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as express from 'express';
 import * as http from 'http';
 import * as cors from 'cors';
+import * as swaggerUi from 'swagger-ui-express';
+import { AdminModule } from './admin/admin.module';
+
+async function createSwaggerRouter(
+  app: INestApplication,
+  modules: Function[], // eslint-disable-line @typescript-eslint/ban-types
+): Promise<express.Router> {
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('GeoWeb Referral API')
+    .setVersion('1.0')
+    .setDescription(
+      'The GeoWeb Referral API that allows claims to be referred by users.',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig, {
+    include: modules,
+  });
+
+  const swaggerHtml = swaggerUi.generateHTML(document);
+  const router = express
+    .Router()
+    .use(swaggerUi.serveFiles(document))
+    .get('/', (req, res) => {
+      res.send(swaggerHtml);
+    });
+
+  return router;
+}
 
 async function bootstrap() {
   const server = express();
@@ -37,15 +66,26 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   app.useGlobalPipes(new ValidationPipe());
-  const config = new DocumentBuilder()
-    .setTitle('GeoWeb Referral API')
-    .setDescription(
-      'The GeoWeb Referral API that allows claims to be referred by users.',
-    )
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('doc', app, document);
+
+  // setup swagger
+  const privateSwaggerRouter = await createSwaggerRouter(app, [
+    AppModule,
+    AdminModule,
+  ]);
+  server.use('/doc', (req, res, next) => {
+    switch (req.headers.host) {
+      case 'public':
+        res.sendStatus(404);
+        return;
+      case 'private':
+        privateSwaggerRouter(req, res, next);
+        return;
+      default:
+        // this shouldn't be possible
+        res.sendStatus(500);
+        return;
+    }
+  });
 
   await app.init();
 
